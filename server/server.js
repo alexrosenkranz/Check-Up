ec06006c424144f21b95e27767cdaebea684d50e
thechutrain <chu.chuyalan@gmail.com>
2017-04-18 19:34:26 -0400

    testing works w. synced to testing db

--- server/server.js
+++ server/server.js
@@ -2,39 +2,31 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
-const path = require('path')
-// const errorHandler = require('./controllers/middleware/errorHandler')
+// const path = require('path')
 
 // Create express App ------------------------- /
 const app = express()
+app.disable('x-powered-by')
 const PORT = process.env.PORT || 3001
 
 // require models ------------------------- /
 const db = require('./db/models')
-const apiRouter = require('./controllers/apiRouter')
 
-// Logger ------------------------- /
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms')) // for logging
+// Express only serves static assets in production ... React PART
+// if (process.env.NODE_ENV === 'production') {
+  // app.use(express.static(path.join(__dirname, '/..', '/client/build')))
+// }
 
 // App middleware ------------------------------ /
+app.use(morgan('dev')) // for logging
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 // Route config -------------------------------------------/
-app.use('/api', apiRouter)
-// app.use(errorHandler)
-
-// Express only serves static assets in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/..', '/client/build')))
-  // app.get('/', (req, res) => {
-  //   res.type('.html')
-  //   res.sendFile(path.join(__dirname, '../client/build/index.html'))
-  // })
-}
+app.use('/api', require('./controllers/apiRouter'))
 
 // Start server ---------------------------------- /
-if (process.env.NODE_ENV !== 'test' && process.env.NODE_ENV !== 'travisTest') {
+if (process.env.NODE_ENV !== 'testing') {
   db.sequelize.sync().then(() => {
     console.info('Databases are all synced!')
     app.listen(PORT, (err) => {
@@ -42,10 +34,11 @@ if (process.env.NODE_ENV !== 'test' && process.env.NODE_ENV !== 'travisTest') {
       console.info(`${process.env.NODE_ENV} ENV: Listening on port: ${PORT}`)
     })
   }).catch((err) => console.error(err))
-} else {
-  app.listen(PORT, (err) => {
-    if (err) console.log(err)
-    console.info(`Listening on port: ${PORT}`)
-  })
 }
+// else {
+//   app.listen(PORT, (err) => {
+//     if (err) console.log(err)
+//     console.info(`Listening on port: ${PORT}`)
+//   })
+// }
 module.exports = app
