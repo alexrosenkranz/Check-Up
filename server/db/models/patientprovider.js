d827f20c97b6085c32205aab7bc30cc5f36ef5c9
thechutrain <chu.chuyalan@gmail.com>
2017-04-18 18:28:59 -0400

    patientProviderTable

---
+++ server/db/models/patientprovider.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var PatientProvider = sequelize.define('PatientProvider', {
+    Patient_id: DataTypes.INTEGER,
+    Provider_id: DataTypes.INTEGER
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return PatientProvider;
+};
