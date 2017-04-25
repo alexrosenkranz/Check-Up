346a7ad8c156734c3c8ba34f2690e89712363ebf
alex@netwaveinteractive.com <alex.rosenkranz@gmail.com>
2017-04-25 10:41:04 -0400

    updating nativebase

--- mobileClient/App.js
+++ mobileClient/App.js
@@ -3,6 +3,11 @@ import { StyleSheet, Text, View, Navigator } from 'react-native';
 import SignIn from './components/signin/SignIn'
 
 export default class App extends React.Component {
+  // async componentWillMount() {
+  //     await Expo.Font.loadAsync({
+  //       'Roboto': require('native-base/Fonts/Roboto.ttf'),
+  //       'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf')
+  //     });
   render() {
     return (
       <SignIn/>
--- mobileClient/components/signin/SignIn.js
+++ mobileClient/components/signin/SignIn.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import { Container, Content, Form, Item, Input, Label} from 'native-base'
+import { View, StyleSheet } from 'react-native'
+import { Container, Content, Form, Item, Input, Label, Button, Text} from 'native-base'
 import { Constants } from 'expo'; 
 
 export default class SignIn extends React.Component {
@@ -8,11 +8,16 @@ export default class SignIn extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-    
-          <Item rounded>
-            <Input placeholder='Rounded Textbox'/>
-          </Item>
         
+          <Item rounded>
+            <Input placeholder='Email'/>
+          </Item>
+          <Item rounded>
+            <Input placeholder='Password'/>
+          </Item>
+          <Button dark rounded>
+            <Text>Dark</Text>
+          </Button>
       </View>
     )
   }
@@ -26,6 +31,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     paddingTop: Constants.statusBarHeight,
     backgroundColor: '#fff'
+  }, 
+  button: {
+    color: '#fff'
   }
 })
 
