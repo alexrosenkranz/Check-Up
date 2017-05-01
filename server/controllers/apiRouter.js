d8acec89d699de29b4a208aa0477c33dc91c9870
thechutrain <chu.chuyalan@gmail.com>
2017-05-01 16:57:40 -0400

    cleaned up code

--- server/controllers/apiRouter.js
+++ server/controllers/apiRouter.js
@@ -4,6 +4,9 @@ const router = express.Router()
 const Query = require('./apiQueries')
 const usersOnly = require('../auth/usersOnly')
 
+// Make sure users are signed in before they can make an api calls
+// router.use(usersOnly())
+
 router.get('/test', usersOnly(), (req, res) => {
   res.json({
     test: true,
@@ -42,57 +45,5 @@ router.post('/new-patient', (req, res) => {
     res.json(err)
   })
 })
-
-/** ======  add providers + appointments etc Queries ==========
- *
- */
-// router.post('/new-appointment', (req, res) => {
-
-// })
-
-// /** ======  Providers Queries ==========
-//  * GET api/v1/providers --> returns all providers info
-//  */
-// router.get('/all-providers', (req, res) => {
-//   Query.findAllProviders().then((results) => {
-//     res.json(results)
-//   })
-// })
-
-// router.post('/new-provider', (req, res) => {
-//   const { first_name, last_name, specialty, phone } = req.body
-//   const providerData = { first_name, last_name, specialty, phone }
-//   Query.addProvider(providerData).then((result) => {
-//     res.json(result)
-//   })
-// })
-
-// /** ======  PatientProvider Queries ==========
-//  * Should be able to:
-//  * 1) add new patient provider relationship
-//  * 2) remove a patient provider relationship
-//  */
-
-// /**
-//  * @return result {object} - has key of result, created. If error, returns a key of error, and erro msg in result.
-//  */
-// router.post('/new-patient-provider', (req, res) => {
-//   const { patientId, providerId } = req.body
-//   Query.addPatientProvider(patientId, providerId).then((result) => {
-//     res.json(result)
-//   })
-// })
-
-// /**
-//  * @return result {object} - either has key of error: 'error msg', or key of success: true
-//  */
-// router.post('/remove-patient-provider', (req, res) => {
-//   const { patientId, providerId } = req.body
-//   // TO DO: autentication, make sure user's id === patientId or something like that
-//   // so you can't destroy someone else's data
-//   Query.removePatientProvider(patientId, providerId).then((result) => {
-//     res.json(result)
-//   })
-// })
 
 module.exports = router
