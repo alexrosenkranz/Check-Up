fc6724e620b2a307c9bc55366fd3982ca523bc08
Alex Rosenkranz <alex.rosenkranz@gmail.com>
2017-05-02 16:51:35 -0400

    React (#22)
    * added Client/React starter structure
    
    * up to date with master
    
    * react native file start
    
    * react native
    
    * updated readme
    
    * changing folder to mobileClient
    
    * added folder structure to native
    
    * native base bug fixing
    
    * updating nativebase
    
    * making sign in / sign ups
    
    * form flow
    
    * working with dashboard and sidebar navigation
    
    * dashboard styling
    
    * updating styles, working on getting sidebar to work correctly with navigator
    
    * merged with master
    
    * added sign up capabilities with api
    
    * pushing so i can sync computers, dropbox isn't cutting it
    
    * checking out PR
    
    * adding forms
    
    * making add provider form
    
    * add providers file
    
    * updated signup form
    
    * updated gitignore
    
    * working on returning user session info
    
    * removed .env
    
    * removed yarn.lock
    
    * cleaning files

---
+++ mobileClient/App.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import { Platform, StyleSheet, Text, View, Navigator } from 'react-native'
+import SignIn from './components/signin/SignIn'
+import SignUp from './components/signin/SignUp'
+import AddAppt from './components/appointments/AddAppt'
+import Main from './components/Main'
+import SideBar from './components/Sidebar';
+import {
+  StackNavigator,
+} from 'react-navigation'
+import { Drawer } from 'native-base'
+import Expo, {Constants, Components} from 'expo'
+
+
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      isReady: false,
+      isLoggedIn: false
+    }
+  }
+  renderScene = (route, navigator, passProps) => {
+    if(route.name == 'SignIn') {
+      return <SignIn refs={this.refs} navigator={navigator} />
+    }
+    if(route.name == 'Dashboard') {
+      return <Main refs={this.refs} navigator={navigator} userInfo={route.userInfo}/>
+    }
+    if (route.name == 'SignUp') {
+      return <SignUp refs={this.refs} navigator={navigator}  />
+    }
+    if (route.name == 'AddAppt') {
+      return <AddAppt refs={this.refs} navigator={navigator} userInfo={route.userInfo} />
+    }
+  }
+async componentWillMount() {
+  if (Platform.OS === 'android') {
+    await Expo
+      .Font
+      .loadAsync({'Roboto': require('native-base/Fonts/Roboto.ttf'), 'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf')});
+
+    this.setState({isReady: true});
+  }
+  this.setState({isReady: true})
+}
+
+_closeDrawer = () => {
+  this
+    .drawer
+    ._root
+    .close()
+};
+
+_openDrawer = () => {
+  console.log('open')
+  this
+    .refs
+    .drawer
+    .open()
+};
+
+
+
+ _navigate = (route) => {
+    this.refs.navigator.push(route);
+    this.refs.drawer.close();
+}
+  // PUT SOMETHING IN HERE TO ROUTE NAVIGATOR TO DASHBOARD IF LOGGED IN 
+
+  render () {
+if (!this.state.isReady) {
+  return <Expo.AppLoading/>;
+}
+    
+    return (
+      <Drawer
+        ref={(ref) => { this.drawer = ref; }}
+        content={<SideBar _navigate={this._navigate} _openDrawer={this._openDrawer}/>}
+        onClose={() => this._closeDrawer()}>
+       <Navigator ref='navigator'
+       _openDrawer= {this._openDrawer}
+        initialRoute={{
+          name: 'SignIn'
+        }}
+        renderScene={this.renderScene}
+        />
+      </Drawer>
+    );
+  }
+}
+
