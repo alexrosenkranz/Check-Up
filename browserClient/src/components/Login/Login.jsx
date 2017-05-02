a34c4c6ddcbf31ff5dd23c9239bfc10270831f29
thechutrain <chu.chuyalan@gmail.com>
2017-05-02 18:09:27 -0400

    not really working

--- browserClient/src/components/Login/Login.jsx
+++ browserClient/src/components/Login/Login.jsx
@@ -1,5 +1,27 @@
 import React from 'react'
+import { FormGroup, FormControl, ControlLabel } from 'react-bootstrap'
 
-const Login = () => (<h1>You are at the Login page</h1>)
+// (<h1>You are at the Login page</h1>)
+const Login = () => (
+  <form>
+    <FormGroup
+      controlId="formBasicText"
+      validationState={this.getValidationState()}
+    >
+      <ControlLabel>Working example with validation</ControlLabel>
+      <FormControl
+        type="text"
+        value={this.state.value}
+        placeholder="Enter text"
+        onChange={this.handleChange}
+      />
+      <FormControl.Feedback />
+      <HelpBlock>Validation is based on string length.</HelpBlock>
+    </FormGroup>
+  </form>
+)
+// class Login extends Component {
+
+// }
 
 export default Login
